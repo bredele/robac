039147142443eab2ada116286db84e65db094b6c
bredele <olivier.wietrich@gmail.com>
2019-02-28 12:46:32 -0700

    Execute callback for given path without roles

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
+/**
+ * Dependencies.
+ */
 
+const fs = require('fs')
+const {join} = require('path')
+const jsonwebtoken = require('jsonwebtoken')
+const {parse} = require('url')
 
-module.exports = () => {
-  return (req, handler) => {
+module.exports = (folder, secret) => {
+  const tree = (req, handler) => {
+    const pathname = parse(req.url).pathname
+    const token = authorization(req)
 
+    if (!tree[pathname]) handler()
+    else {
+      if (token) {
+        jsonwebtoken.verify(token, secret, function(err, decoded) {
+          console.log('decoded', err, decoded)
+        })
+      }
+    }
   }
+  if (folder) tree['/'] = roles(folder)
+  return walk(folder, folder, tree)
+}
+
+/**
+ * Get authorization token.
+ *
+ * @param {Object} req
+ * @api private
+ */
+
+function authorization (req) {
+  const header = req.headers.authorization
+  if (header) {
+    const authorization = header.split(' ')
+    if (authorization[0] === 'Bearer' && authorization.length === 2) return authorization[1]
+  }
+}
+
+/**
+ * Walk folder and add pathnames to object tree.
+ *
+ * @param {String} root
+ * @param {String} folder
+ * @param {Object} tree
+ * @api private
+ */
+
+function walk (root, folder, tree) {
+  if (folder) {
+    fs.readdirSync(folder).map(file => {
+      const path = join(folder, file)
+      if (fs.statSync(path).isDirectory()) {
+        const pathname = path.substring(root.length)
+        tree[pathname] = roles(path)
+        walk(root, path, tree)
+      }
+    })
+  }
+  return tree
+}
+
+/**
+ * Generate roles based on pathname and roles.json file.
+ *
+ * @param {String} file
+ * @return {Object}
+ * @api private
+ */
+
+function roles (file) {
+  let obj = {}
+  try {
+    obj = require(join(file, 'roles.json'))
+  } catch (e) {}
+  return obj
 }
--- test/robac.js
+++ test/robac.js
@@ -4,8 +4,65 @@
 
 const test = require('tape')
 const robac = require('..')
+const {join} = require('path')
+const jsonwebtoken = require('jsonwebtoken')
 
-test('should return a function', assert => {
+test('should return a middleware function', assert => {
   assert.plan(1)
   assert.equal(typeof robac(), 'function')
 })
+
+test('should parse folder and generate pathnames', assert => {
+  assert.plan(3)
+  const handler = robac(join(__dirname, 'pages'))
+  assert.equal(!!handler['/'], true)
+  assert.equal(!!handler['/a'], true)
+  assert.equal(!!handler['/b'], true)
+})
+
+test('should attach roles.json to pathname', assert => {
+  assert.plan(3)
+  const handler = robac(join(__dirname, 'pages'))
+  assert.deepEqual(handler['/'], {})
+  assert.deepEqual(handler['/b'], {
+    roles: ['admin', 'super']
+  })
+  assert.deepEqual(handler['/a'], {})
+})
+
+test('should execute callback only if authorization header but pathname does not have roles', assert => {
+  assert.plan(1)
+  const handler = robac(join(__dirname, 'pages'), 'thisisasecret')
+  const req = {
+    url: '/c',
+    headers: {
+      authorization: `Bearer ${token()}`
+    }
+  }
+  handler(req, () => assert.ok('success'), 'thisisasecret')
+})
+
+// test('should execute callback only if authorization header contains allowed role', assert => {
+//   assert.plan(1)
+//   const handler = robac(join(__dirname, 'pages'), 'thisisasecret')
+//   const req = {
+//     url: '/b',
+//     headers: {
+//       authorization: `Bearer ${token()}`
+//     }
+//   }
+//   handler(req, () => assert.ok('success'), 'thisisasecret')
+// })
+
+/**
+ * Generate test token.
+ *
+ * @return {String}
+ * @api private
+ */
+
+function token () {
+  return jsonwebtoken.sign({
+    roles: ['super']
+  }, 'thisisasecret')
+}
